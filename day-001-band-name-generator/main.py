3249efa54294f3de95032abd1616df1dcb04174f
whizboy297 <114996844+whizboy297@users.noreply.github.com>
2026-08-05 15:13:45 +0100

    Day 1: build band name generator

---
+++ day-001-band-name-generator/main.py
@@ -0,0 +1,44 @@
+# This is a simple Python script that prints "Hello World!" to the console.
+#print("Hello World!")
+
+# String Manipulation
+# You can manipulate strings in Python using various methods. Here are some examples:
+#print("Hello World!\nThis is a new line.\nThis is an indented line.")
+
+# String Concatenation
+# You can concatenate strings using the + operator.
+#print("Hello" + " " + "Angela!")
+
+#Input
+# You can take user input using the input() function.
+#input("Please enter your name: ")
+
+#print("Hello " + input("Please enter your name: ")+ "!")
+
+#Variables
+# You can store values in variables and use them later in your code.
+#username = input("Please enter your name: ")
+#print("Hello " + username + "!")
+
+#Check length of string
+# You can check the length of a string using the len() function.
+#length = len(username)
+#print("Your name has " + str(length) + " characters.")
+
+#Variable Naming
+# Variable names should be descriptive and follow certain rules. They can contain letters, numbers, and underscores, but cannot start with a number. They should not be Python reserved keywords.
+# Example of valid variable names:
+#user_name = "Angela"
+#userAge = 25
+
+
+#Band Name Generator Project
+# This is a simple project that generates a band name based on user input.
+# It takes the name of the city you grew up in and the name of your pet,
+# and combines them to create a band name.
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
+
